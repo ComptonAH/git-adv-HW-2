94f4b391d71edc2f0157ec7c59557aa56baeb7b3
Nikita Karetkin <nikita-kareta@yandex.ru>
2023-06-29 13:05:10 +0600

    init commit

---
+++ test.py
@@ -0,0 +1,10 @@
+N = input("Enter a three-digit number: ")
+
+if len(N) != 3:
+    print("You must to enter a three-digit number")
+else:
+    N = int(N)
+    a = N//100
+    b = (N//10) % 10
+    c = N % 10
+    print(sum([a, b, c], 0))
